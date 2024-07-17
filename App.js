de0900ae1340d54a1f0c346ba8af4dd27bc292a4
shhiivvaam <life.shivam2394@gmail.com>
2024-07-18 00:06:45 +0530

    Adding User Auth Token Caching

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import LoginScreen from './app/Screens/login/LoginScreen';
 
 // Clerk
 import { ClerkProvider, SignedIn, SignedOut } from '@clerk/clerk-expo';
+import * as SecureStore from 'expo-secure-store';
 
 SplashScreen.preventAutoHideAsync();
 
@@ -15,6 +16,50 @@ const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
 if (!publishableKey) {
   throw new Error('Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env')
 }
+
+const tokenCache = {
+  async getToken(key) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (error) {
+      console.error("SecureStore get item error: ", error);
+      return null;
+    }
+  },
+  async saveToken(key, value) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
+
+
+// const tokenCache = {
+//   async getToken(key) {
+//     try {
+//       const item = await SecureStore.getItemAsync(key);
+//       if (item) {
+//         console.log(`${key} was used 🔐 \n`);
+//       } else {
+//         console.log("No values stored under key: " + key);
+//       }
+//       return item;
+//     } catch (error) {
+//       console.error("SecureStore get item error: ", error);
+//       await SecureStore.deleteItemAsync(key);
+//       return null;
+//     }
+//   },
+//   async saveToken(key, value) {
+//     try {
+//       return SecureStore.setItemAsync(key, value);
+//     } catch (err) {
+//       return;
+//     }
+//   },
+// };
 
 export default function App() {
   const [loaded, error] = useFonts({
@@ -34,7 +79,7 @@ export default function App() {
   }
 
   return (
-    <ClerkProvider publishableKey={publishableKey}>
+    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
       <View style={styles.container}>
         <SignedIn>
           <Text>You are signed in....</Text>
